176a82318fc3e6075f0045a6a78993c0f9ffcb4e
pconesa <p.conesa.mingo@gmail.com>
2020-04-30 06:23:00 +0200

    just indentation of iedb

--- add_iedb.js
+++ add_iedb.js
@@ -2,33 +2,33 @@
 
 var add_iedb = function(d){
 	var n = 1;
-        var m = 0;
+	var m = 0;
 	if(__external_data['iedb']){
-                var count = {}
+	  var count = {}
 		var __epitopes = ["EPITOPES",[]];
-                var type = "LINEAR_EPITOPE";
-                var tag = 0;
+	  var type = "LINEAR_EPITOPE";
+	  var tag = 0;
 		__external_data['iedb'].forEach(function(i){
-                        for(var j=i['start'];j<=i['end'];j++){
-                          if(j in count){
-                            count[j]++;
-                          }else{
-                            count[j]=1;
-                          }
-                          if(count[j]>m)m=count[j];
-                        }
+      for(var j=i['start'];j<=i['end'];j++){
+        if(j in count){
+          count[j]++;
+        }else{
+          count[j]=1;
+        }
+        if(count[j]>m)m=count[j];
+      }
 			__epitopes[1].push({color:'#83BE00',type:type,begin:i['start'],end:i['end'],description:'Linear epitope',internalId:'iedb_'+n,evidences:
 				{
 					"Imported information":[{url:'http://www.iedb.org/epitope/'+i['evidence'],id:i['evidence'],name:'Imported from IEDB'}]
 				}
 			});
 			n++;
-                        if(m>3){
-                          m = 0;
-                          count = {};
-                          tag++;
-                          type = "LINEAR_EPITOPE_"+tag;
-                        }
+        if(m>3){
+          m = 0;
+          count = {};
+          tag++;
+          type = "LINEAR_EPITOPE_"+tag;
+        }
 		});
 		d.push( __epitopes );
 	}
