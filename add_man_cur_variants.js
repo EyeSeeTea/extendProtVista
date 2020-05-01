f4ab9cc9792ca2460863e1bd1c9225b8b0755bff
pconesa <p.conesa.mingo@gmail.com>
2020-05-01 17:35:42 +0200

    fix syntax error again

--- add_man_cur_variants.js
+++ add_man_cur_variants.js
@@ -36,24 +36,25 @@ var add_man_cur_variants = function (data) {
 
 };
 
-function customizeVariants(tooltip){
+function customizeVariants(tooltip) {
   let annot = tooltip.data;
 
   // Add a Genomic data:
-  tooltip.addSimpleRow('Genomic data',annot.originalGenomic + " muted to " + annot.newGenomic);
+  tooltip.addSimpleRow('Genomic data', annot.originalGenomic + " muted to " + annot.newGenomic);
   // Protein effect
-  tooltip.addSimpleRow( 'Protein data',  annot.wildtype + " muted to " + annot.variation + "<br/>" + annot.reportedProtChange);
+  tooltip.addSimpleRow('Protein data', annot.wildtype + " muted to " + annot.variation + "<br/>" + annot.reportedProtChange);
   // Reported cases
-  tooltip.addSimpleRow('Reported cases',annot.numberOfViruses);
+  tooltip.addSimpleRow('Reported cases', annot.numberOfViruses);
   // Data source
-  if (annot.reference){
+  if (annot.reference) {
     var reference = "";
     var icon = '';
-    if (annot.refIcon){
+    if (annot.refIcon) {
       icon = '<img src="' + annot.refIcon + '" width="16" height="16">';
-    };
+    }
     reference = icon + '&nbsp;<a href="' + annot.reference + '" target="_blank">' + annot.reference + '</a>';
     tooltip.addSimpleRow('Data source', reference);
-};
+  }
+}
 
 module.exports = add_man_cur_variants;
