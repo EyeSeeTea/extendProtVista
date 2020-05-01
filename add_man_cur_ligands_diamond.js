c86c625c0c1dfcdf24f70d8da401a53e2a0fb29b
Jose R Macias <jr.macias.bioinf@gmail.com>
2020-05-01 01:07:50 +0000

    Add enriched descriptions to annotations panel

--- add_man_cur_ligands_diamond.js
+++ add_man_cur_ligands_diamond.js
@@ -18,6 +18,11 @@ var add_man_cur_ligands_diamond = function (data) {
           if (track.data.forEach) {
             track.data.forEach(function (feat) {
               // console.log("->>> DRUG_SCREENING reading .__cvData.track.data.feat", feat);
+              if (track.reference) {
+                feat.description = feat.description + '<br><br><b>Data source:</b>'
+                  + '&nbsp;&nbsp;<img src="' + track.fav_icon + '" width="16" height="16">'
+                  + '&nbsp;&nbsp;<a href="' + track.reference + '" target="_blank">' + track.reference + '</a>'
+              };
               resItems.push(feat);
             });
           };
