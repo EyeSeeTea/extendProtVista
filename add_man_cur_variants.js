f450e9bd43f3575847f9275851f076927652a9df
pconesa <p.conesa.mingo@gmail.com>
2020-05-01 17:31:50 +0200

    use new addSimpleRow in tooltip.

--- add_man_cur_variants.js
+++ add_man_cur_variants.js
@@ -3,37 +3,25 @@
 var add_man_cur_variants = function (data) {
 
   let resItems = [];
-  let sourceurl = "https://bigd.big.ac.cn/ncov/variation/annotation";
   let resCat = ["Genomic_Variants", resItems];
-  let accession = __alignment.uniprot;
 
-  // chech if we can load annotations for this accession
+  // check if we can load annotations for this accession
   if (__cvData == null) return;
 
   if (__cvData.forEach) {
     __cvData.forEach(function (track) {
       if (track != null)
-        if (track.track_name == "Cncb_variants") {
-          var data = track.data;
-          // console.log("->>> CNCB_VARIANTS reading .__cvData.track");
+        if (track.track_name === "Cncb_variants") {
           if (track.data.forEach) {
             track.data.forEach(function (feat) {
-              feat.description = feat.mutationType + " : " + feat.mutationEffect + " @ " + feat.genomicPosition
-                + "<br>" + "Prot. change: " + feat.reportedProtChange
-                + "<br>" + feat.wildtype + " " + feat.variation
-                + "<br>" + "Reported cases: " + feat.numberOfViruses;
+              feat.description = feat.mutationType + " : " + feat.mutationEffect + " @ " + feat.genomicPosition;
               feat.type = feat.mutationEffect;
-              // console.log("->>> CNCB_VARIANTS writing .__cvData.track.data.feat", feat);
               if (track.reference) {
-                var icon_link = "";
-                if (track.fav_icon) {
-                  var icon_link = '&nbsp;&nbsp;<img src="' + track.fav_icon + '" width="16" height="16">'
-                };
-                feat.description = feat.description + '<br><br><b>Data source:</b>'
-                  + icon_link
-                  + '&nbsp;&nbsp;<a href="' + track.reference + '" target="_blank">' + track.reference + '</a>'
+                feat.reference = track.reference;
               };
-
+              if (track.fav_icon) {
+                feat.refIcon = track.fav_icon;
+              };
               // Extend tooltip
               feat.extendTooltip = customizeVariants;
 
@@ -51,11 +39,21 @@ var add_man_cur_variants = function (data) {
 function customizeVariants(tooltip){
   let annot = tooltip.data;
 
-  // Add a Genomic info line:
-  let genRow = tooltip.table.append('tr');
-  genRow.append('td').text('Genomic data');
-  genRow.append('td').text(annot.originalGenomic + " muted to " + annot.newGenomic);
-
+  // Add a Genomic data:
+  tooltip.addSimpleRow('Genomic data',annot.originalGenomic + " muted to " + annot.newGenomic);
+  // Protein effect
+  tooltip.addSimpleRow( 'Protein data',  annot.wildtype + " muted to " + annot.variation + "<br/>" + annot.reportedProtChange;
+  // Reported cases
+  tooltip.addSimpleRow('Reported cases',annot.numberOfViruses);
+  // Data source
+  if (annot.reference){
+    var reference = "";
+    var icon = '';
+    if (annot.refIcon){
+      icon = '<img src="' + annot.refIcon + '" width="16" height="16">';
+    };
+    reference = icon + '&nbsp;<a href="' + annot.reference + '" target="_blank">' + annot.reference + '</a>';
+    tooltip.addSimpleRow('Data source', reference);
 };
 
 module.exports = add_man_cur_variants;
